a65a0989986d10a544224a3915cb76b6d8e41fb1
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 21:43:54 -0600

    v4xx: cleanup packages
    * Remove packages that are unused, already included in build or vendor
    
    Change-Id: I5f0569f206fbd3e78e4a332ef90c3dcf6876e09c

--- v4xx.mk
+++ v4xx.mk
@@ -104,10 +104,6 @@ PRODUCT_PACKAGES += \
     memtrack.msm8226 \
     liboverlay
 
-# Filesystem
-PRODUCT_PACKAGES += \
-    e2fsck
-
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8226
@@ -216,10 +212,6 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf
-
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
 
 # Wifi
 PRODUCT_PACKAGES += \
