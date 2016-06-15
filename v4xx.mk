fe8d3c8dee30e03864abeb56513a6281a3c590c5
Sultanxda <sultanxda@gmail.com>
2016-06-15 10:30:02 -0700

    v4xx: Add GPS configs
    From stock V40010C package.

--- v4xx.mk
+++ v4xx.mk
@@ -104,8 +104,12 @@ PRODUCT_PACKAGES += \
     liboverlay
 
 # GPS
-PRODUCT_PACKAGES += \
-    gps.msm8226
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
